7858403cdc6cd9b5cec3956af190bb1867de9cde
Darrin Holst <darrinholst@gmail.com>
2012-03-30 15:06:56 -0500

    make it work

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # ExceptionsToHipchat
 
-TODO: Write a gem description
+Sends exceptions to HipChat
 
 ## Installation
 
@@ -8,22 +8,13 @@ Add this line to your application's Gemfile:
 
     gem 'exceptions_to_hipchat'
 
-And then execute:
-
-    $ bundle
-
-Or install it yourself as:
-
-    $ gem install exceptions_to_hipchat
-
 ## Usage
 
-TODO: Write usage instructions here
+Rails 3: add as rack middleware to `application.rb'
 
-## Contributing
+    config.middleware.use ExceptionsToHipchat::Notifier,
+        :api_token => "YOUR TOKEN",
+        :room => "YOUR ROOM",
+        :user => "USER MESSAGE WILL COME FROM (MAX 15 CHARACTERS)",
+        :notify => Rails.env.production?
 
-1. Fork it
-2. Create your feature branch (`git checkout -b my-new-feature`)
-3. Commit your changes (`git commit -am 'Added some feature'`)
-4. Push to the branch (`git push origin my-new-feature`)
-5. Create new Pull Request
--- lib/exceptions_to_hipchat/notifier.rb
+++ lib/exceptions_to_hipchat/notifier.rb
@@ -4,17 +4,30 @@ module ExceptionsToHipchat
   class Notifier
     def initialize(app, options = {})
       @app = app
-      @client HipChat::Client.new(options[:api_token] || raise "HipChat API token is required")
-      @room = options[:room] || raise "HipChat room is required"
+      @client = HipChat::Client.new(options[:api_token] || raise("HipChat API token is required"))
+      @room = options[:room] || raise("HipChat room is required")
       @color = options[:color] || :red
       @notify = options[:notify]
+      @user = (options[:user] || "Notifier")[0..15]
     end
 
     def call(env)
       @app.call(env)
     rescue Exception => exception
-      @client[@room].send("foo", exception.class, :color => @color, :notify => @notify)
+      send_to_hipchat exception
       raise exception
+    end
+
+    def send_to_hipchat(exception)
+      begin
+        @client[@room].send(@user, message_for(exception), :color => @color, :notify => @notify)
+      rescue => hipchat_exception
+        $stderr.puts "\nWARN: Unable to send message to HipChat: #{hipchat_exception}\n"
+      end
+    end
+
+    def message_for(exception)
+      "[#{exception.class}] #{exception}"
     end
   end
 end
