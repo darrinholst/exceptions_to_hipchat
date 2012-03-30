ab920ce42d6c9da031f1bfc32a0ae467c6aeea3c
Darrin Holst <darrinholst@gmail.com>
2012-03-30 14:08:31 -0500

    add constructor and rack call

--- lib/exceptions_to_hipchat/notifier.rb
+++ lib/exceptions_to_hipchat/notifier.rb
@@ -1,2 +1,14 @@
-class ExceptionsToHipchat::Notifier
+module ExceptionsToHipchat
+  class Notifier
+    def initialize(app, options = {})
+      @app, @options = app, options
+    end
+
+    def call(env)
+      @app.call(env)
+    rescue Exception => exception
+      p exception
+      raise exception
+    end
+  end
 end
